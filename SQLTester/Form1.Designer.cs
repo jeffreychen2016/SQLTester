2f2654a0ba29fc46877a32b7dd0012e4859502d7
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-21 15:57:59 -0500

    added connections and binded data to gridview

--- SQLTester/Form1.Designer.cs
+++ SQLTester/Form1.Designer.cs
@@ -31,8 +31,8 @@
             this.grdRecord = new System.Windows.Forms.DataGridView();
             this.txtCommand = new System.Windows.Forms.TextBox();
             this.btnExecute = new System.Windows.Forms.Button();
-            this.txt = new System.Windows.Forms.Label();
             this.lblCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdRecord)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,57 +41,61 @@
             this.grdRecord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grdRecord.Location = new System.Drawing.Point(45, 12);
             this.grdRecord.Name = "grdRecord";
-            this.grdRecord.Size = new System.Drawing.Size(706, 150);
+            this.grdRecord.Size = new System.Drawing.Size(706, 227);
             this.grdRecord.TabIndex = 0;
             // 
             // txtCommand
             // 
-            this.txtCommand.Location = new System.Drawing.Point(45, 213);
+            this.txtCommand.Location = new System.Drawing.Point(45, 261);
+            this.txtCommand.Multiline = true;
             this.txtCommand.Name = "txtCommand";
-            this.txtCommand.Size = new System.Drawing.Size(100, 20);
+            this.txtCommand.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCommand.Size = new System.Drawing.Size(542, 170);
             this.txtCommand.TabIndex = 1;
             // 
             // btnExecute
             // 
-            this.btnExecute.Location = new System.Drawing.Point(625, 194);
+            this.btnExecute.Location = new System.Drawing.Point(625, 261);
             this.btnExecute.Name = "btnExecute";
             this.btnExecute.Size = new System.Drawing.Size(126, 39);
             this.btnExecute.TabIndex = 2;
             this.btnExecute.Text = "&Execute Statement";
             this.btnExecute.UseVisualStyleBackColor = true;
-            // 
-            // txt
-            // 
-            this.txt.AutoSize = true;
-            this.txt.Location = new System.Drawing.Point(622, 263);
-            this.txt.Name = "txt";
-            this.txt.Size = new System.Drawing.Size(73, 13);
-            this.txt.TabIndex = 3;
-            this.txt.Text = "Record Count";
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
             // 
             // lblCount
             // 
             this.lblCount.AutoSize = true;
             this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCount.Location = new System.Drawing.Point(716, 263);
+            this.lblCount.Location = new System.Drawing.Point(121, 242);
             this.lblCount.Name = "lblCount";
             this.lblCount.Size = new System.Drawing.Size(16, 16);
             this.lblCount.TabIndex = 4;
             this.lblCount.Text = "0";
             this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Record Count";
+            // 
             // frmTester
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblCount);
-            this.Controls.Add(this.txt);
             this.Controls.Add(this.btnExecute);
             this.Controls.Add(this.txtCommand);
             this.Controls.Add(this.grdRecord);
             this.Name = "frmTester";
             this.Text = "SQL Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmFormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grdRecord)).EndInit();
             this.ResumeLayout(false);
@@ -104,8 +108,8 @@
         private System.Windows.Forms.DataGridView grdRecord;
         private System.Windows.Forms.TextBox txtCommand;
         private System.Windows.Forms.Button btnExecute;
-        private System.Windows.Forms.Label txt;
         private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SQLTester/Form1.cs
+++ SQLTester/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace SQLTester
 {
     public partial class frmTester : Form
     {
+        OleDbConnection conn;
+
         public frmTester()
         {
             InitializeComponent();
@@ -19,7 +22,43 @@ namespace SQLTester
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            var connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\jchen\source\repos\SQLTester\Books.accdb;
+                                Persist Security Info = False;";
 
+            conn = new OleDbConnection(connString);
+            conn.Open();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            OleDbCommand command = null;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            DataTable table = new DataTable();
+
+            try
+            {
+                command = new OleDbCommand(txtCommand.Text, conn);
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+
+                // binding data
+                grdRecord.DataSource = table;
+                lblCount.Text = table.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error In SQL Command", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            command.Dispose();
+            adapter.Dispose();
+            table.Dispose();
+        }
+
+        private void frmFormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Close();
+            conn.Dispose();
         }
     }
 }
